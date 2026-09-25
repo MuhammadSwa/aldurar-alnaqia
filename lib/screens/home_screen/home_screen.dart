d3a24c8848181d30fb590dfb6a3e127795f6db8e
MuhammadSwa <muhammadatefsw@protonmail.com>
2026-09-25 16:59:00 +0300

    remove stray }

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -121,7 +121,7 @@ class _HomePageState extends ConsumerState<HomePage> {
             AppTile(
               title: dalayilAlkhayratCollection[dayIndex].title,
               // maxTitleLines: 1,
-              subtitle: 'ورد يوم ${arabicWeekdays[dayIndex]}}',
+              subtitle: 'ورد يوم ${arabicWeekdays[dayIndex]}',
               leading: const AppTileLeadingIcon(
                 icon: Icons.auto_stories_rounded,
               ),
